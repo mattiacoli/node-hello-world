a219f02efdc74adfaecc45ffb060e3908268d9f2
mattiacoli <mattia.coli@aol.com>
2025-03-11 13:07:46 +0100

    add process.argv in index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log('questo é un testo scritto utilizzando echo')
 console.log('questa é una seconda stringa utilizzando echo')
+
+
+const testoDaSostituire = process.argv[2]
+
+console.log(testoDaSostituire);
